775cfb082c9f72a16141e1806f61948448d87960
anandka <anandkarwa92@gmail.com>
2018-03-08 15:47:16 +0530

    modification of the script

--- consul/custom.sh
+++ consul/custom.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# export $DNSENTRIES="server=/consul/127.0.0.1#8600"
+service=dnsmasq
+
+# export DNSENTRIES="server=/consul/127.0.0.1#8600"
 echo "The DNSENTRIES environment variable is $DNSENTRIES"
-#Support other distributions 
+
+#Support other distributions
+# todo upstart/systemd/initd
 
 YUM_CMD=$(which yum 2>/dev/null ) 
 APT_GET_CMD=$(which apt-get 2>/dev/null )
@@ -15,30 +19,51 @@ elif [[ ! -z $YUM_CMD ]]; then
   package="yum"
 else
   echo "unknown platform"
-  echo $(uname -a)
+  echo $(uname -a) 
 fi
 
-if [ $package = "apt-get" ]; then
-  echo "installing dnsmasq on ubuntu"
-  apt-get install dnsmasq -y 
-  echo $DNSENTRIES > /etc/dnsmasq.d/10-consul
-  service dnsmasq restart
-  #service dnsmasq status
-  #runlevel set
-  echo "installing dnsmasq on ubuntu completed"
+
+if service --status-all 2>&1 | grep $service; then
+	echo "$service is installed"
+else
+	echo "$service is not installed"
+
+	if [ $package = "apt-get" ]; then
+	  echo "installing $service  on ubuntu"
+	  apt-get install $service -y  || echo " $service installation failed"
+	  update-rc.d $service enable
+	  #runlevel set
+	  echo "$service  installed"
+	fi
+
+	if [ $package = "yum" ]; then
+	  echo "installing $service  on centos"
+	  yum install $service -y  || echo " $service installation failed"
+	  #why was below line there?
+	  #yum install systemd -y
+	  systemctl enable $service.service
+	  echo "$service  installed"
+	fi
+
 fi
 
-if [ $package = "yum" ]; then
-  echo "installing dnsmasq on centos"
-  yum install dnsmasq -y
-  yum install systemd -y
-  echo $DNSENTRIES > /etc/dnsmasq.d/10-consul
-  systemctl enable dnsmasq.service
-  systemctl restart dnsmasq.service
-  #systemctl status dnsmasq.service
-  echo "installing dnsmasq on centos completed"
+
+echo "setting up configuration for dnsmasq"
+echo $DNSENTRIES > /etc/dnsmasq.d/10-consul
+
+
+if (( $(ps -ef | grep -v grep | grep $service | wc -l) > 0 ))
+then
+echo "$service is running!!!"
+echo "restarting $service..."
+	  service $service restart
+else
+#/etc/init.d/$service start
+echo "$service is stopped"
+echo "starting $service..."
+	  service $service start
 fi
+
 
 sleep 10
-
 
